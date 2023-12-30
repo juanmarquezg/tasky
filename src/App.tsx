4dc3abe8275fc26227b60ce2c721416056b57169
juanmarquezg <jcmarquezgalvis@gmail.com>
2023-12-30 18:24:02 -0500

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo2.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logox" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Tasky, administra tus tareas
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
